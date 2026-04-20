d9b67579896e74d1b2584100593cbbc339843634
CalebeXimenes07 <rcalebesximenes@gmail.com>
2026-04-20 18:25:40 -0300

    fix: correct client name display and loan ToString implementation

--- BibliotecaPoo/Entities/Cliente.cs
+++ BibliotecaPoo/Entities/Cliente.cs
@@ -3,7 +3,7 @@
     public string Cpf { get; private set; }
     public string Nome { get; private set; }
 
-    public Cliente(string cpf, string nome)
+    public Cliente(string nome, string cpf)
     {
         Cpf = cpf;
         Nome = nome;
